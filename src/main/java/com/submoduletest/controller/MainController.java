05e14b84eba3812054dbc18561cecc533218822d
ssseongmin <pose1358@naver.com>
2023-12-11 15:52:32 +0900

    t

---
+++ src/main/java/com/submoduletest/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.submoduletest.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    public String main () {
+        return "index";
+    }
+}
